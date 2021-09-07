11d071ea4d5ba28484196c89e232dedeb21d526d
Red911 <mromain911@gmail.com>
2021-09-07 19:32:55 +0200

    Le personne regarde vers le curseur

--- QuotesJam/Assets/Script/PlayerController.cs
+++ QuotesJam/Assets/Script/PlayerController.cs
@@ -7,11 +7,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed = 20.0f;
     [SerializeField] private float limiter = 0.7f;
     private Vector3 movement;
+    private Vector3 mousePos;
     private Rigidbody rb;
+    public Camera mainCamera;
+    
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
     }
 
     private void FixedUpdate()
@@ -24,11 +28,26 @@ public class PlayerController : MonoBehaviour
         }
 
         rb.velocity = new Vector3(movement.x * speed, movement.y, movement.z * speed);
+        
+        
     }
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.z = Input.GetAxisRaw("Vertical");
+        
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLenght;
+
+        if(groundPlane.Raycast(cameraRay, out rayLenght))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLenght);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.red);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+
     }
 }
