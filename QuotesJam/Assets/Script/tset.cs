391e131a67969e62c0ad8c1713ce594f4e2f3ac3
Red911 <mromain911@gmail.com>
2021-09-07 15:25:47 +0200

    J'ai modif test

--- QuotesJam/Assets/Script/tset.cs
+++ QuotesJam/Assets/Script/tset.cs
@@ -7,7 +7,7 @@ public class tset : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Souper le test");
     }
 
     // Update is called once per frame
