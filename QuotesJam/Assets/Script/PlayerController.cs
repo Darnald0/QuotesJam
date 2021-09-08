8c5fe0a5b595d20303c74988da825ae35590c41e
Darnald0 <55183284+Darnald0@users.noreply.github.com>
2021-09-08 10:58:25 +0200

    add kick

--- QuotesJam/Assets/Script/PlayerController.cs
+++ QuotesJam/Assets/Script/PlayerController.cs
@@ -6,12 +6,23 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 20.0f;
     [SerializeField] private float limiter = 0.7f;
+    [SerializeField] private float kickRange = 1.0f;
+    [SerializeField] private float kickCoolDown = 1.0f;
+    [SerializeField] private float kickDuration = 0.5f;
+
+    private float cd;
+    private Collider kickHitBox;
     private Vector3 movement;
     private Rigidbody rb;
+    private bool isKicking = false;
+    private float timeKickEnd;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        kickHitBox = transform.GetChild(0).GetComponent<BoxCollider>();
+        Debug.Log(kickHitBox);
+        cd = kickCoolDown;
     }
 
     private void FixedUpdate()
@@ -30,5 +41,35 @@ public class PlayerController : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.z = Input.GetAxisRaw("Vertical");
+
+        cd -= Time.deltaTime;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (cd <= 0)
+            {
+                isKicking = true;
+
+                timeKickEnd = Time.time + kickDuration;
+            }
+        }
+
+        if (isKicking)
+        {
+            Kick();
+        }
+    }
+
+    private void Kick()
+    {
+        if (Time.time > timeKickEnd)
+        {
+            cd = kickCoolDown;
+            kickHitBox.enabled = false;
+            isKicking = false;
+        } else
+        {
+            kickHitBox.enabled = true;
+        }
     }
 }
