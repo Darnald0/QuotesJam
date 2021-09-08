1abd68fc79cc02b010372065f6ca49b202ecff7e
Red911 <mromain911@gmail.com>
2021-09-08 15:11:38 +0200

    l'ennemis peut taper

---
+++ QuotesJam/Assets/Script/DetectionMelee.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DetectionMelee : MonoBehaviour
+{
+    public bool detectionPlayer = false;
+    private DateTime nextDamage;
+    public float fightAfterTime;
+    void Start()
+    {
+        nextDamage = DateTime.Now;
+    }
+
+    
+    void FixedUpdate()
+    {
+        if(detectionPlayer == true)
+        {
+            FightInDetection();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            detectionPlayer = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            detectionPlayer = false;
+        }
+    }
+
+    public void FightInDetection()
+    {
+        if(nextDamage <= DateTime.Now)
+        {
+            Debug.Log("Touché");
+            nextDamage = DateTime.Now.AddSeconds(System.Convert.ToDouble(fightAfterTime));
+        }
+    }
+}
