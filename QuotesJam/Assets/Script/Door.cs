c29d1d3e24f85cc53a561f1a261610bf3d9023cb
Darnald0 <55183284+Darnald0@users.noreply.github.com>
2021-09-10 12:01:12 +0200

    cocerdvdvvsdfv

--- QuotesJam/Assets/Script/Door.cs
+++ QuotesJam/Assets/Script/Door.cs
@@ -28,7 +28,7 @@ public class Door : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Enemy" && isGettingKick)
+        if (collision.gameObject.tag == "Enemy")
         {
             EnnemyLife ennemy = collision.gameObject.GetComponent<EnnemyLife>();
             ennemy.Die(1);
